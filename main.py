537cc8ee2433cffe32e6c4258b72858dcbd7512f
andrianm28 <andrianmaulana28@gmail.com>
2021-04-15 05:35:46 +0700

    add timezone

--- main.py
+++ main.py
@@ -3,6 +3,11 @@ from pydantic import BaseModel, Field
 # from model import train, convert, predict
 import databases, sqlalchemy, datetime, uuid
 from typing import List
+import pytz
+
+# Timezone
+
+tz = pytz.timezone('Asia/Jakarta')
 
 ## Postgres Database
 DATABASE_URL = "postgres://podbrtrvupzwrs:3ae577a84fe32e348dd104312d8d6cd7a60b90e063c3803cad90c8b341278892@ec2-34-233-0-64.compute-1.amazonaws.com:5432/d8mf2fka342hhv"
@@ -70,7 +75,7 @@ async def fetch_energies():
 @app.post("/energies", response_model=Energies, status_code=201)
 async def create_energy(entry: EnergyEntry):
     gID     = str(uuid.uuid1())
-    gDate   = str(datetime.datetime.now())
+    gDate   = str(datetime.datetime.now(tz=tz))
     query   = energies.insert().values(
         id          = gID,
         created_at  = gDate,
